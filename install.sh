1fb28497732c0f006c01857bf110b90c9c327c90
Henning Erlandsen <henning.erlandsen@mojang.com>
2020-09-10 22:55:33 +0200

    Make install script prepare directories

--- install.sh
+++ install.sh
@@ -2,15 +2,17 @@
 
 function install {
 
-    read -p "Install $1?" -n 1 -r
+    read -p "Install $1? " -n 1 -r
     echo    # (optional) move to a new line
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
+        DIR=${2%/*}
+        mkdir --parents "$DIR"
         ./link.bat "$2" "$1"
     fi
 }
 
-install git/.gitconfig ~/.gitconfig
-install git/git-prompt.sh ~/.config/git/git-prompt.sh
+install ./git/.gitconfig ~/.gitconfig
+install ./git/git-prompt.sh ~/.config/git/git-prompt.sh
 install .bashrc ~/.bashrc
 install windows-terminal/settings.json ~/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json
