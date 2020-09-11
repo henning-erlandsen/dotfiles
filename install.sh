3edc5f42ecab384629bf6efd9350b6046731a7e8
Henning Erlandsen <henning.erlandsen@mojang.com>
2020-09-11 12:14:43 +0200

    Add prompt to overwrite existing files

--- install.sh
+++ install.sh
@@ -2,10 +2,22 @@
 
 function install {
 
+    # Ask to install file
     read -p "Install $1? " -n 1 -r
     echo    # (optional) move to a new line
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        # Does the file already exist?
+        if [[ -f "$2" ]]; then
+            read -p "$2 already exists. Overwrite? " -n 1 -r
+            echo
+            # Move old file to .orig
+            if [[ $REPLY =~ ^[Yy]$ ]]; then
+                mv "$2" "$2".orig
+            else
+                return
+            fi
+        fi
+
         DIR=${2%/*}
         mkdir --parents "$DIR"
         ./link.bat "$2" "$1"
